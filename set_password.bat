5bcfe0c8b94bfd7955c709010c2a9056cead3b5e
Nsfr750 <nsfr750@gmail.com>
2025-09-04 17:21:28 +0200

    1.0.0

--- set_password.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Setting up Password Manager Master Password
-echo ====================================
-
-set PYTHONPATH=%~dp0
-"%PYTHONPATH%venv\Scripts\python.exe" "%PYTHONPATH%scripts\set_master_pw_cli.py"
